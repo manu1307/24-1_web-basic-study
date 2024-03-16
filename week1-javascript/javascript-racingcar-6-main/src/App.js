f7e2c8d0607d7c4d73f7599d66b81689c44297f2
Sung Chul Park <jasonsc@naver.com>
2024-03-16 22:27:46 +0900

    fix : add getNames format change
    - string -> array using split

--- week1-javascript/javascript-racingcar-6-main/src/App.js
+++ week1-javascript/javascript-racingcar-6-main/src/App.js
@@ -9,7 +9,7 @@ class App {
     const names = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)."
     );
-    names.forEach((name) => {
+    names.split(",").forEach((name) => {
       if (!this.validateName(name))
         throw new Error("[ERROR] 이름은 5자 이하만 가능합니다.");
     });
