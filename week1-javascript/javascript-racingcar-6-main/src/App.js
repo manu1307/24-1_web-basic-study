32533131dc66b7f20b5a28d6728ac71fa44459c2
Sung Chul Park <jasonsc@naver.com>
2024-03-16 22:01:51 +0900

    feat : get name and round values
    Add 3 methods
    - get names
    - name validation check
    - get rounds

--- week1-javascript/javascript-racingcar-6-main/src/App.js
+++ week1-javascript/javascript-racingcar-6-main/src/App.js
@@ -1,5 +1,30 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  async play() {
+    await this.getNames();
+    await this.getRound();
+  }
+
+  async getNames() {
+    const names = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)."
+    );
+    names.forEach((name) => {
+      if (!this.validateName(name))
+        throw new Error("[ERROR] 이름은 5자 이하만 가능합니다.");
+    });
+  }
+
+  validateName(name) {
+    if (name.length > 5) return false;
+    return true;
+  }
+
+  async getRound() {
+    const round = await MissionUtils.Console.readLineAsync(
+      "시도할 회수는 몇회인가요?"
+    );
+  }
 }
 
 export default App;
