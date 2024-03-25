12a090e47d037d2f6d4eb53c7e04b7b1088fb031
박성철(Park Sungchul)(jasonsc) <jasonsc@ncsoft.com>
2024-03-25 18:05:20 +0900

    feat : Add lotto class in app

--- week1-javascript/javascript-lotto-6-main/src/App.js
+++ week1-javascript/javascript-lotto-6-main/src/App.js
@@ -1,5 +1,18 @@
+import Lotto from "./Lotto";
+
 class App {
-  async play() {}
+  constructor() {
+    this.lotto = new Lotto();
+  }
+
+  async play() {
+    this.lotto.getPurchaseAmount();
+    this.lotto.printLotteryNumbers();
+    this.lotto.getWinningNumbers();
+    this.lotto.getBonusNumber();
+    this.lotto.checkCorrectNumbers();
+    this.lotto.printResult();
+  }
 }
 
 export default App;
